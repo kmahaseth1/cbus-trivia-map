4fdf11ca37bbbe50b1da59c8146b66f34d7b607c
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-05-04 17:16:53 -0400

    [Update] Update CWD and file paths
    Update the current working directory and file paths of the excel datasets and output to conform to the update directory structure of the project
    
    ---
    
    [Fix] Amend the code extract full google maps URL
    
    Add code to ensure that extraction of full URL stops based on number of rows for each day of the week
    
    ---
    
    [Feature] Extract latitude and longitude data from full google map URL
    
    Add a loop to parse latitude and longitude from the full URL and append them to new lists.
    
    ---
    
    [Update] Update the main DataFrame
    
    Add latitutude and logitude data from the lists as new columns of the primary DataFrame

--- scripts/cbus_trivia_map.py
+++ scripts/cbus_trivia_map.py
@@ -2,12 +2,13 @@ import folium
 import os
 import pandas as pd
 import calendar as cd
+import re
 
-# Update the current working directory
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# Update the current working directory to the main project folder
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # Create DataFrames for each day
-df = pd.read_excel('cbus_trivia_list_updated.xlsx')
+df = pd.read_excel('data/cbus_trivia_list_updated.xlsx')
 
 df_mon = df.iloc[:11, 0:6]
 df_tue = df.iloc[:57, 7:13]
@@ -20,7 +21,7 @@ df_sun = df.iloc[14:20, 28:34]
 # Update column headers
 dfs = [df_mon, df_tue, df_wed, df_thu, df_fri, df_sat, df_sun]
 days = list(cd.day_name)
-constant_cols = ['Bar', 'Company', 'Host', 'Time', 'Address', 'Address 2']
+constant_cols = ['Bar', 'Company', 'Host', ' Start Time', 'Address', 'Address 2']
 
 # Add day column to each DataFrame
 for i, df in enumerate(dfs):
@@ -31,9 +32,19 @@ for i, df in enumerate(dfs):
 cbus_trivia_df = pd.concat(dfs, ignore_index=True)
 cbus_trivia_df = cbus_trivia_df[cbus_trivia_df['Bar'].notna()]
 
-# Extract coordinates from maps link
+# Extract coordinates from the full url
+lat = []
+lon = []
+for i in cbus_trivia_df['Address 2']:
+    match = re.search(r"@(-?\d+\.\d+),(-?\d+\.\d+)", i)
+
+    if match:
+        lat.append(match.group(1))
+        lon.append(match.group(2))
 
 # Add latitude and longitude columns to the DataFrame
+cbus_trivia_df['Latitude'] = lat
+cbus_trivia_df['Longitude'] = lon
 
 # Create the base map
 # Columbus's Coordinates
@@ -47,4 +58,4 @@ cbus_trivia_df = cbus_trivia_df[cbus_trivia_df['Bar'].notna()]
 # Add filters
 
 # Save the map
-#cbus_map.save("cbus_map.html")
+#cbus_map.save("output/cbus_map.html")
--- scripts/update_xl.py
+++ scripts/update_xl.py
@@ -4,11 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
-# Update the current working directory
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# Update the current working directory to the main project folder
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Read the data on trivia list
-trivia_wb = xl.load_workbook('cbus_trivia_list.xlsx')
+# Read the trivia list data
+trivia_wb = xl.load_workbook('data/cbus_trivia_list.xlsx')
 sheet = trivia_wb.active
 
 # Set up Selenium
@@ -16,7 +16,7 @@ options = Options()
 options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 
-# Create two additional columns for each trivia night event 
+# Create two additional columns for each trivia event 
 # One for initial URL and another for the full URL
 link_cols = [1, 6, 11, 16, 21]
 
@@ -29,20 +29,39 @@ for col in reversed(link_cols):
     sheet.insert_cols(new_idx2)
     sheet.cell(row=1, column=new_idx2).value = 'Full URL'
 
+    num_empty_rows = 0
+    custom_max_row = sheet.max_row
     for row in range(2, sheet.max_row + 1):
         source_cell = sheet.cell(row=row, column=col)
+
+        if not source_cell.value:
+           num_empty_rows += 1
+        else:
+           num_empty_rows = 0
+
+        if num_empty_rows >=4:
+            custom_max_row = row - 4
+            break
+
+    for row in range(2, custom_max_row+ 1):
+        source_cell = sheet.cell(row=row, column=col)
+        initial_url = None
+
         if source_cell.hyperlink:
             initial_url = source_cell.hyperlink.target
             sheet.cell(row=row, column=new_idx).value = initial_url
 
         try:
+            # If the initial URL exists, retrieve the full URL
             if initial_url:
                 driver.get(initial_url)
                 time.sleep(2)
                 full_url = driver.current_url
                 sheet.cell(row=row, column=new_idx2).value = full_url
+            else:
+                sheet.cell(row=row, column=new_idx2).value = ''
         except Exception as e:
             sheet.cell(row=row, column=new_idx2).value = f"Error: {str(e)}"
 
-# Save the workbook
-trivia_wb.save('cbus_trivia_list_updated.xlsx')
+# Save the new columns in a new workbook
+trivia_wb.save('data/cbus_trivia_list_updated.xlsx')
