8cdb55a8e90a9a87899e4221f23dbcfe10bbff0e
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-05-11 18:45:57 -0400

    Update data source for the map
    Use the final excel dataset with manually updated coordinates instead of cbus_map_df from create_pd.py to create markers.

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,13 @@
 
 import folium
 import os
-from create_pd import cbus_trivia_df
+import pandas as pd
 
 # Update the current working directory to the main project folder
 os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Read the updated excel workbook
+final_df = pd.read_excel('data/final_trivia_list.xlsx')
 
 # Create the base map
 # Columbus's Coordinates
@@ -14,9 +17,9 @@ cbus_coords = [39.971178, -82.998795]
 cbus_map = folium.Map(location = cbus_coords, zoom_start = 11.15)
 
 # Create FeatureGroup to add markers and day based filter
-for day in cbus_trivia_df['Day'].unique():
+for day in final_df['Day'].unique():
     feature_group = folium.FeatureGroup(name = day)
-    day_df = cbus_trivia_df[cbus_trivia_df['Day'] == day]
+    day_df = final_df[final_df['Day'] == day]
 
     for i, row in day_df.iterrows():
         label_html = f"""
