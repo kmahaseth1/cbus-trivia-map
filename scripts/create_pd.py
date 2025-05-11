70f958accff72b5b6d3325d597c941220be63402
kmahaseth1 <136269836+kmahaseth1@users.noreply.github.com>
2025-05-11 12:41:19 -0400

    Separate main script into two scripts
    Split the script that creates the final DataFrame and generates the Folium map (cbus_trivia_map.py) into create_pd.py which constructs the primary DataFrame for the map and main.py which produces the Folium map.

--- scripts/create_pd.py
+++ scripts/create_pd.py
@@ -45,35 +45,3 @@ for i in cbus_trivia_df['Address 2']:
 # Add latitude and longitude columns to the DataFrame
 cbus_trivia_df['Latitude'] = lat
 cbus_trivia_df['Longitude'] = lon
-
-# Create the base map
-# Columbus's Coordinates
-cbus_coords = [39.971178, -82.998795]
-
-# Create the map
-cbus_map = folium.Map(location = cbus_coords, zoom_start = 11.15)
-
-# Create FeatureGroup to add markers and day based filter
-for day in cbus_trivia_df['Day'].unique():
-    feature_group = folium.FeatureGroup(name = day)
-    day_df = cbus_trivia_df[cbus_trivia_df['Day'] == day]
-
-    for i, row in day_df.iterrows():
-        label_html = f"""
-        <b>Bar Name:</b> {row['Bar']}<br>
-        <b>Trivia Day:</b> {row['Day']}<br>
-        <b>Company:</b> {row['Company']}<br>
-        <b>Start Time:</b> {row['Start Time']}
-        """
-        folium.Marker(
-            location=[row['Latitude'], row['Longitude']],
-            popup=folium.Popup(label_html, max_width=500)
-        ).add_to(feature_group)
-    
-    feature_group.add_to(cbus_map)
-
-# Add checkbox
-folium.LayerControl(collapsed=False).add_to(cbus_map)
-
-# Save the map
-cbus_map.save("output/cbus_map.html")
